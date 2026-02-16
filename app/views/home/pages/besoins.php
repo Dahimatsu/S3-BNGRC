7fb824247540e22a2926e49086f0010dc7070f74
Youssi <andrianoayoussi@gmail.com>
2026-02-16 23:16:11 +0300

    fix page besoin

---
+++ app/views/home/pages/besoins.php
@@ -0,0 +1,38 @@
+<header class="mb-0">
+    <h2 class="fw-bold title">SAISIE DE BESOIN<span style="color: var(--lime-color)">.</span></h2>
+    <p class="text-muted small">Page de formulaire de saisie.</p>
+</header>
+<main class="container mt-5">
+    <section class="row g-5">
+        <article class="col-md-12">
+            <div class="don-card bg-white p-4 h-100 rounded shadow-sm">
+                <div class="d-flex align-items-center mb-3">
+                    <h3 class="fw-black mb-0">FORMULAIRE</h3>
+                </div>
+                <p class="mb-4">Saisir les nouveaux besoins.</p>
+
+                <select name="" id="" class="form-select mb-3 ">
+                    <option value="">Type de besoin</option>
+                    <option value="">Besoin 1</option>
+                    <option value="">Besoin 2</option>
+                    <option value="">Besoin 3</option>
+                </select>
+
+                <select name="" id="" class="form-select mb-3 ">
+                    <option value="">Catégorie</option>
+                    <option value="">Catégorie 1</option>
+                    <option value="">Catégorie 2</option>
+                    <option value="">Catégorie 3</option>
+                </select>
+
+                <input type="number" class="form-control mb-3" placeholder="Quantité de besoin">
+
+                <a href="#"
+                    class="btn-lime w-100 text-center text-decoration-none">
+                    ENREGISTRER UN BESOIN
+                </a>
+            </div>
+        </article>
+
+    </section>
+</main>
